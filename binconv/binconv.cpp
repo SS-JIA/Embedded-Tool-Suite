297f516fe9fc92f8fe1450b54e6969c43c432556
jiasicheng614@gmail.com <jiasicheng614@gmail.com>
2016-09-28 02:57:39 -0400

    Add help message for binconv

--- binconv/binconv.cpp
+++ binconv/binconv.cpp
@@ -30,7 +30,6 @@ string tohex(uint64_t target)
 
 void print(uint64_t target)
 {
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     int test;
     int reglength = log2((double)target);
     cout << "hex: " << uppercase << setw(28) << ("0x" + tohex(target)) << "    ";
@@ -137,17 +136,43 @@ void convert(char* target, char base, char form)
     return;
 }
 
-int main(int argc, char* argv[]) 
+int helptext() 
+{
+    cout << "BINCONV - A binary converter for embedded applications" << endl;
+    cout << " " << endl;
+    cout << "usage: binconv [options] [input] [options] [input] [options] [input] ..." << endl;     
+    cout << " " << endl;
+    cout << "NUMBER BASE OPTIONS" << endl;
+    cout << "\t-h\t\tinput is a hexadecimal number" << endl;
+    cout << "\t-b\t\tinput is a binary number (default)" << endl;
+    cout << "\t-d\t\tinput is a decimal number" << endl;
+    cout << " " << endl;
+    cout << "BINARY NUMER OPTIONS" << endl;
+    cout << "\t-v\t\tinput is a binary number" << endl;
+    cout << "\t-p\t\tinput is a list of numbers indicating which bits are 1 (e.g. -p 3,5 is interpreted as 101000)" << endl;
+    cout << "\t-r\t\tinput is a range of numbers indicating which bits are 1 (e.g. -r 3,5 is interpreted as 111000)" << endl;
+}
+int main(int argc, char* argv[])
 {
     //cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     char inptype = 'b';
     char inpform = 'p';
     uint8_t size = 32;
+    uint8_t arglen = 0;
+    string longarg;
+    bool firstline = true;
     for (int i=1; i < argc; i++)
     {
+        arglen = strlen(argv[i]);
         if (argv[i][0] == '-') 
         {
-            for (int j=1; j < strlen(argv[i]); j++)
+            if (arglen > 1 && argv[i][1] == '-')
+            {
+                longarg = argv[i];
+                if (longarg == "--help")
+                    return helptext();
+            }
+            for (int j=1; j < arglen; j++)
             {
                 if (argv[i][j] == 'h' || argv[i][j] == 'd' || argv[i][j] == 'b')
                 {
@@ -161,6 +186,11 @@ int main(int argc, char* argv[])
         }
         else
         {
+            if (firstline)
+            {
+                cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                firstline = false;
+            }
             convert(argv[i], inptype, inpform);
         }
     }
